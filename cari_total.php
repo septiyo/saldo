f3eaeb3c42ad78f77428a2c2d0f41cd7a9d93527
septiyo <septiyou@gmail.com>
2017-01-10 09:39:30 +0700

    cabang1

--- cari_total.php
+++ cari_total.php
@@ -8,23 +8,22 @@ ini_set('display_errors', '0');
 echo json_encode($res); */
 
 
-$sql = "SELECT * FROM saldo_gh";
-
+$sql = "SELECT SUM(HARGA)AS HARGA, SUM(TOTAL)AS TOTAL FROM saldo_gh;";
 $hasil = mysqli_query($mysqli, $sql);
 //$results = array();
 
 
-//foreach($hasil as $row){
+/* foreach($hasil as $row){ */
 
 while($row=mysqli_fetch_assoc($hasil)){
 	
-			$results[] = array(
-					'ID'=> $row[ID],
-					'TGL_ORDER' => $row[TGL_ORDER],
-					'ITEM' => $row[ITEM],
+			$results= array(
+					'TOT_HARGA'=> $row[HARGA],
+					'TOT_TOTAL' => $row[TOTAL],
+				/* 	'ITEM' => $row[ITEM],
 					'HARGA' => $row[HARGA],
 					'ONGKIR' => $row[ONGKIR],
-					'TOTAL' => $row[TOTAL],
+					'TOTAL' => $row[TOTAL], */
 
 			);
 			
