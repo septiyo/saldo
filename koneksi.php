c54727d413de8cd5876bbb065320e350e85f8b6d
septiyo <septiyou@gmail.com>
2017-01-24 11:11:14 +0700

    commit1

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,9 @@
 <?php
 
-
-$mysqli = mysqli_connect("localhost","root","","saldo") or die('gagalx');
+ 
+  $mysqli = mysqli_connect("localhost","root","","saldo") or die('gagalx');
+  
+   
+   
+   
 ?>
